f3c5935262feaecef30ae8d5ff49a826f12aacda
Sharath Gururaj <e271828r@gmail.com>
2021-02-26 19:37:23 +0530

    foo

--- capstone.py
+++ capstone.py
@@ -1,76 +1,195 @@
 import json
 import math
 import numpy as np
+from datetime import date
+from dateutil.relativedelta import relativedelta  # $ pip install python-dateutil
+
 
 def main():
-    for segment in segments:
-        cx, cy = segments[segment]["pos"]
-        x = np.outer(np.linspace(cx-3.54, cx+3.54, 30), np.ones(30))
-        y = x.copy().T
-        zipped = zip(np.reshape(x, -1), np.reshape(y, -1))
+    testSimulate2()
 
-        def mapper(p):
-            x, y = p
-            return position(segment, x, y)
-        z = np.reshape(np.fromiter(map(mapper, zipped), float), (30, 30))
-    # a = np.linspace(0, 10, 10)
-    # b = a.copy()
-    # c = zip(a, b)
-    print("hi")
-        # z = position(segment, x, y)
 
+M = 1000000
+K = 1000
+
+"""
+aut has to be 3, 4, or 5
+"""
+
+
+def simulate(oldPrice, oldPerf, oldSz, oldMtbf, oldCap, oldAut, startAgeYears, price, perf, sz, mtbf, cap, aut,
+             segment):
+    totalLifeYears = 2
+    rndYears = rndTimeYears(oldAut)
+    for month in range(1, totalLifeYears * 12 + 1):
+        curAge = currentAgeYears(month, rndYears, startAgeYears, oldPerf == perf and oldSz == sz)
+        tperf, tsz, tmtbf = oldPerf, oldSz, oldMtbf
+        tcap, taut = oldCap, oldAut
+        if month / 12 > rndYears:
+            tperf, tsz, tmtbf = perf, sz, mtbf
+        if month / 12 > 1:
+            tcap, taut = cap, aut
+        # TODO
+
+
+def testSimulate2():
+    x = simulate2()
+    # perfX, szY, rndYears = zip(*x)
+    # print(perfX)
+
+
+
+def simulate2():
+    seg = segments["low"]
+    ebbPerf, ebbSz, ebbMtbf = 3, 17, 14000
+    perff = np.linspace(ebbPerf - 5, ebbPerf + 5, 20)
+    szz = np.linspace(ebbSz - 5, ebbSz + 5, 20)
+    ll = []
+    dummy = 0
+    for perf in perff:
+        # if perf != 3:
+        #     continue
+        for sz in szz:
+            if perf <= 0 or sz <= 0:
+                continue
+            # if sz != 17:
+            #     continue
+            for mtbf in range(seg["mtbf"][0], seg["mtbf"][1] + 1000, 1000):
+                d = dist(ebbPerf, ebbSz, perf, sz)
+                rndYear = rndTimeYears(5, d, abs(ebbMtbf - mtbf), 3)
+                if rndYear < 1:
+                    dummy += 1
+                    for price in np.linspace(*seg["price"], 20):
+                        totalScore = 0
+                        for month in range(1, 24):
+                            seg2 = segmentState(month, seg)
+                            age = currentAgeYears(month, rndYear, 4.6)
+                            aware = awareness(month, rndYear)
+                            tperf, tsz, tmtbf = ebbPerf, ebbSz, ebbMtbf
+                            if month >= rndYear*12:
+                                tperf, tsz, tmtbf = perf, sz, mtbf
+                            sc = score(seg2, age, price, tperf, tsz, tmtbf, aware, .5)
+                            cost = totalVariableCostExistingProduct(tperf, tsz, tmtbf, 5, .31, month)
+                            totalScore += sc*cost
+                        ll.append(totalScore)
+                        # if totalScore >450:
+                        #     ll.append((perf, sz, rndYear*12))
+                        #     print("dist:{} perf:{} sz:{} mtbf:{} price:{} rndTimeMonths:{} totalScore:{}".format(d, perf, sz, mtbf, price, rndYear*12, totalScore))
+    return ll
+
+
+
+# tested
+def testRndEbb():
+    start = date(2021, 12, 31)
+    perf, sz, mtbf = 4, 17, 14 * K
+    deltaYears = rndTimeYears(5, dist(perf, sz, 3, 17), abs(mtbf - 14 * K), 1)
+    print(deltaYears)
+    print("revision date is ", start + relativedelta(days=int(deltaYears * 365)))
+    print("cost is ", rndCost(deltaYears))
+
+
+# TODO
+def segmentState(month, segment):
+    seg = segment.copy()
+    year = month / 12
+    p = seg["pos"]
+    driftPerYear = seg["driftPerYear"]
+    seg["pos"] = [p[0] - year * driftPerYear[0], p[1] - year * driftPerYear[1], \
+                  p[2] - year * driftPerYear[0], p[3] - year * driftPerYear[1]]
+    if year > 1:
+        seg["demand"] *= ((1 + seg["growth"]) ** year)
+        seg["price"] = [seg["price"][0] - 0.5 * int(year), seg["price"][1] - 0.5 * int(year)]
+    return seg
+
+
+# tested
+def testCurrentAge():
+    for month in range(1, 12):
+        print(month, currentAgeYears(month, .59, 4.6))
+
+
+"""
+skipRevision = True if the device is not really revised due to no change in position
+"""
+
+
+def currentAgeYears(month, rndYears, startAgeYears, skipRevision=False):
+    if month / 12 < rndYears or skipRevision:
+        return startAgeYears + month / 12.0
+    else:
+        ageAtRevision = (startAgeYears + rndYears) / 2
+        ageAfterRevision = month / 12 - rndYears
+        return ageAtRevision + ageAfterRevision
 
 
 segments = {
     "traditional": {
         "age": [[-1, 0, 0, 0.1], [0, 2, 0.45, 0.1], [2, 4, -.45, 1.9], [4, 11, 0, 0.1]],
         "price": [20, 30],
-        "pos": [5, 15],
-        "mtbf": [12000, 17000],
-        "weights": [.47, .23, .21, .09]
+        "pos": [5, 15, 5, 15],
+        "mtbf": [14000, 19000],
+        "weights": [.47, .23, .21, .09],
+        "growth": .092,
+        "driftPerYear": [.7, -.7],
+        "demand": 7387 * K
     },
     "low": {
         "age": [[-1, 0, 0, 0.1], [0, 2, 0.02, 0.08], [2, 7, 0.176, -0.232], [7, 11, -0.176, 2.232]],
         "price": [15, 25],
-        "pos": [1.7, 18.3],
+        "pos": [2.5, 17.5, 1.7, 18.3],
         "mtbf": [12000, 17000],
-        "weights": [.47, .23, .21, .09]
+        "weights": [.47, .23, .21, .09],
+        "growth": .117,
+        "driftPerYear": [.5, -.5],
+        "demand": 8960 * K
 
     },
     "high": {
         "age": [[0, 0.7, -4 / 25.0, 1], [.7, 2.4, -359 / 1000, 11393 / 10000], [2.4, 3.4, -1117 / 10000, 27289 / 50000],
                 [3.4, 11, 0, 1.5 / 9]],
         "price": [30, 40],
-        "pos": [8.9, 11.1],
+        "pos": [7.5, 12.5, 8.9, 11.1],
         "mtbf": [20000, 25000],
-        "weights": [.47, .23, .21, .09]
-
+        "weights": [.47, .23, .21, .09],
+        "growth": .162,
+        "driftPerYear": [.9, -.9],
+        "demand": 2554 * K,
     },
     "performance": {
         "age": [[0, 1, 0.2, 0.8], [1, 3.5, -0.36, 1.36], [3.5, 11, 0, 0.1]],
         "price": [25, 35],
-        "pos": [9.4, 16.0],
+        "pos": [8.0, 17.0, 9.4, 16.0],
         "mtbf": [22000, 27000],
-        "weights": [.47, .23, .21, .09]
+        "weights": [.47, .23, .21, .09],
+        "growth": .198,
+        "driftPerYear": [1, -.7],
+        "demand": 1915 * K
 
     },
     "size": {
         "age": [[0, 1.5, 0.33, 0.5], [1.5, 4, -0.36, 1.54], [4, 11, 0, 0.1]],
         "price": [25, 35],
-        "pos": [4.0, 10.6],
+        "pos": [3.0, 12.0, 4.0, 10.6],
         "mtbf": [16000, 21000],
-        "weights": [.47, .23, .21, .09]
-
+        "weights": [.47, .23, .21, .09],
+        "growth": .183,
+        "driftPerYear": [.7, -1.0],
+        "demand": 1984 * K
     }
 }
 
 
-def age(p, x):
-    return y(segments[p]["age"], x)
+# tested
+def ageScore(segment, x):
+    return y(segment["age"], x)
 
 
-def price(p, x):
-    l, u, = segments[p]["price"]
+# tested
+def priceScore(segment, x):
+    l, u, = segment["price"]
+    if x > u:
+        return 0
     return (x - u) / (l - u)
 
 
@@ -78,86 +197,166 @@ def price(p, x):
 decision variables
 m:       related to MTBF. It is the lower end percentage
 pc:      position constant: the decrease in score per unit distance inside the fine cut circle
-         pc has to be less than 1/2.5 
+         pc has to be less than 1/2.5 = 0.4
+         
 awr:     awareness is a fraction between 0 and 1. it costs $ to buy awareness
 acs:     accessibility is a fraction between 0 and 1. it costs $ to buy accesibility. 
          It is set per product, but it accumulates to the segment.
 """
 
-m = 0.8
-pc = 0.08
+m = 0.7
+pc = 0.75 / 2.5
 awr = 0.8
 acs = .5
 
+awarenessCurve = [[0, .52], [1, .52 - .33 / 12 + .4 / 12]]
 
-def mtbf(p, x):
+
+def awareness(month, revisionTimeYears):
+    baseAwareNess = yy(awarenessCurve, month, 0, "extrapolate")
+    newsWorthy = 0
+    if month > revisionTimeYears * 12:
+        baseAwareNess += .25
+    baseAwareNess = min(1, baseAwareNess)
+    return baseAwareNess
+
+
+def mtbfScore(segment, x):
     global m
-    l, u = segments[p]["mtbf"]
-    segmentsLocal = [[l-5000, 0], [l, m], [u, 1]]
-    return yy(segmentsLocal, x)
+    l, u = segment["mtbf"]
+    segmentsLocal = [[l - 5000, 0], [l, m], [u, 1]]
+    return yy(segmentsLocal, x, defaultRight=1, defaultLeft=0)
 
 
-def position(p, perf, sz):
-    a, b = segments[p]["pos"]
-    d = dist(a, b, perf, sz)
-    if d > 2.5:
-        slope = (2.5 * pc - 1) / 1.5
-        c = 2.66 - 6.66 * m
-        return line(slope, c, d)
+def testPositionScore():
+    # segment = segmentState(12, "traditional")
+    print(positionScore(segments["traditional"], 3, 17))
+
+
+def positionScore(segment, perf, sz):
+    cx, cy, ix, iy = segment["pos"]
+    d = dist(cx, cy, perf, sz)
+    if d <= 2.5:
+        actualD = dist(ix, iy, perf, sz)
+        return 1 - pc * actualD
+    elif perf == cx:
+        x, y = cx, cy + 2.5
     else:
-        return 1 - pc * d
+        tperf, tsz = perf - cx, sz - cy
+        # p = 2.5 / math.sqrt(1 + tsz * tsz / (tperf * tperf))
+        # q = tsz * p / tperf
+        # x = p + cx
+        # y = q + cy
+        p = 2.5 / math.sqrt(1 + tsz * tsz / (tperf * tperf))
+        if not (min(perf, cx) <= p + cx <= max(perf, cx)):
+            p = -p
+        q = tsz * p / tperf
+        x = p + cx
+        y = q + cy
+
+    distanceFromIdeal = dist(ix, iy, x, y)
+    residualDistance = dist(x, y, perf, sz)
+    scoreAtBoundary = 1 - pc * distanceFromIdeal
+    finalScore = yy([[0, scoreAtBoundary], [1.5, 0]], residualDistance, 0, 0)
+    return finalScore
 
 
-def score(p, agee, pricee, perf, sz, mtbff):
-    ageScore = age(p, agee)
-    priceScore = price(p, pricee)
-    posScore = position(p, perf, sz)
-    mtbfScore = mtbf(p, mtbff)
-    w1, w2, w3, w4 = segments[p]["weights"]
-    return w1 * ageScore + w2 * priceScore + w3 * posScore + w4 * mtbfScore
+def testScore():
+    s = score("traditional", 0, 4.6, 21, 3, 17, 14000, .52, .54)
+    print("final score is ", s)
+    return s
 
+
+def score(segment, agee, pricee, perf, sz, mtbff, awareness, accessibility):
+    ageS = ageScore(segment, agee)
+    priceS = priceScore(segment, pricee)
+    positionS = positionScore(segment, perf, sz)
+    mtbfS = mtbfScore(segment, mtbff)
+    if ageS <= 0 or priceS <= 0 or positionS <= 0 or mtbfS <= 0:
+        return 0
+    w1, w2, w3, w4 = segment["weights"]
+    # print("raw scores age:{} price:{} pos:{}, mtbf:{}".format(ageS, priceS, positionS, mtbfS))
+    baseScore = w1 * ageS + w2 * priceS + w3 * positionS + w4 * mtbfS
+    # print("weighted scores age score:{} priceScore:{} posScore:{} mtbfScore:{}".format(w1 * ageS * 100, w2 * priceS * 100, w3 * positionS * 100, w4 * mtbfS * 100))
+    # print("basecore is ", baseScore)
+    accountsRecievable = 0.993
+    finalScore = baseScore * accountsRecievable * awareness * accessibility
+    return finalScore * 100
+
+
+def testTotalVariableCost():
+    print(totalVariableCostExistingProduct(3, 17, 14000, 5, 0.3, 12))
 
 # the fixed cost only the RnDCost
-def totalVariableCostExistingProduct(perf, sz, mtbfLocal, aut, overProduction):
-    positionMaterialCost(perf, sz) \
-    + mtbfMaterialCost(mtbfLocal) \
-    + labourCost(aut, overProduction)
+def totalVariableCostExistingProduct(perf, sz, mtbfLocal, aut, overProduction, month):
+    pCost = positionMaterialCost(perf, sz)
+    mCost = mtbfMaterialCost(mtbfLocal)
+    lCost = labourCost(aut, overProduction, month)
+    # print(pCost + mCost, lCost)
+    return pCost + mCost + lCost
 
 
 def dist(a, b, perf, sz):
     return np.sqrt((sz - b) * (sz - b) + (perf - a) * (perf - a))
 
 
-positionCostLine = [[0, 11, 1500.0 / 2357.0, 1]]
+def testPositionMaterialCost():
+    print(positionMaterialCost(5.5, 14.5))
+    print(mtbfMaterialCost(17500))
 
 
 def positionMaterialCost(perf, sz):
-    d = dist(1, 1, perf, sz)
-    return y(positionCostLine, d)
+    d = dist(0.732, 19.268, perf, sz)
+    slope = .7456
+    return 1 + slope * d
 
 
 def mtbfMaterialCost(mtbfLocal):
     return mtbfLocal * 0.3 / 1000.0
 
 
-def rndCost(aut):
-    return 1000000 * rndTimeYears(aut)
+def rndCost(timeYears):
+    return M * timeYears
 
 
-labourBaseCost = 10.7 * 1.05
+def testLaborCost():
+    print(labourCost(5, 1))
 
 
-def labourCost(aut, overProduction):
-    labor = 0.1 * (11 - aut) * labourBaseCost * (1 + 1.5 * overProduction) / (1 + overProduction)
-    return labor
+labourBaseCost = 10.7
 
 
-segmentsForAutToYears = [[1, 0.48], [2, 0.49], [3, 0.5], [4, 0.52], [5, 0.6], [6, 0.69], [7, 0.81], [8, 0.99], [9, 1.2],
+def labourCost(aut, overProduction, month):
+    year = int((month+12)/12)
+    labour = labourBaseCost
+    if year == 1:
+        labour = labourBaseCost* 1.05
+    if year == 2:
+        labour = labourBaseCost*1.05*1.05
+    # currently hardcoded to 0.99. In future versions, we will give correct values
+    apLagPenalty = .99
+    cost = 0.1 * (11 - aut) * labour * (1 + 1.5 * overProduction) / (1 + overProduction) / apLagPenalty
+    return cost
+
+
+segmentsForAutToYears = [[1, 0.47], [2, 0.47], [3, 0.48], [4, 0.5], [5, 0.51], [6, 0.69], [7, 0.81], [8, 0.99],
+                         [9, 1.2],
                          [10, 1.48]]
 
+aut3 = [[0, 0], [.1, .17], [.2, .150], [.4, .209], [.6, .295], [1, .513], [2, 1.124], [4, 1.993], [8, 2.532]]
+aut4 = [[0, 0], [.1, .231], [.2, .187], [.4, .246], [.6, .332], [1, .55], [2, 1.161], [4, 2.03], [8, 2.569]]
+aut5 = [[0, 0], [.1, .133], [.2, .248], [.4, .307], [.6, .393], [1, .611], [2, 1.222], [4, 2.091], [8, 2.630]]
 
-def rndTimeYears(aut):
-    return yy(segmentsForAutToYears, aut)
+timePer40KDollar = 40 * K / M
+timePer90KDollar = 90 * K / M
+
+
+def rndTimeYears(aut, dist, deltaMtbf, numProjects=1):
+    autSeg = [aut3, aut4, aut5][aut - 3]
+    distBasedTime = yy(autSeg, dist, defaultRight=2.6)
+    deltaMtbfBasedTime = deltaMtbf * timePer40KDollar / 1000
+    totalTime = (distBasedTime + deltaMtbfBasedTime)
+    return totalTime
 
 
 awarenessSegments = [[0, 0.02, 15000000, 0], [0.02, 0.05, 5000000 / 3.0, 800000 / 3.0],
@@ -169,7 +368,6 @@ def awarenessCost(x):
     return y(awarenessSegments, x)
 
 
-M = 1000000
 accessibilitySegmentMultiple = [[0, 0.05, 15000000, 0], [0.05, 0.22, 125000000 / 17.0, 6500000 / 17.0],
                                 [0.22, 0.275, 100000000 / 11, 0], [0.275, 0.31, 100000000 / 7.0, -10000000 / 7.0],
                                 [0.31, 0.35, 25 * M, -4.7 * M]]
@@ -210,21 +408,44 @@ def y(segmentsLocal, x):
         if l <= x < u:
             return line(m, c, x)
 
+def testYy():
+    month = np.arange(0, 24, 1)
+    print("month", month)
+    segment = "low"
+    cx, cy, ix, iy = segments[segment]["pos"]
+    dx, dy = segments[segment]["driftPerYear"]
+    print(ix, iy)
+    print(dx, dy)
+    segmentX = [[0, ix], [1, ix+dx/12]]
+    segmentY = [[0, iy], [1, iy+dy/12]]
+    print("point0", yy(segmentX, 2, "extrapolate", "extrapolate"))
+    # x = list(map(lambda x: yy(segmentX, x, "extrapolate"), month))
+    # y = list(map(lambda x: yy(segmentY, x, "extrapolate"), month))
+
 
 # segments is a list of points. the function is assumed to be piecewise linear continous function
-# between the points [[x1, y1], [x2, y1], ...]
+# between the points [[x1, y1], [x2, y2], ...]
 # segments [[0, 0], [1, 1], ...]
-def yy(segmentsLocal, x):
+def yy(segmentsLocal, x, defaultLeft=0, defaultRight=0):
     if x < segmentsLocal[0][0]:
-        raise Exception("out of bound")
+        if defaultLeft == "extrapolate":
+            x0, y0 = segmentsLocal[0]
+            x1, y1 = segmentsLocal[0]
+            return y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+        return defaultLeft
     if x == segmentsLocal[0][0]:
         return segmentsLocal[0][1]
     x1, y1 = segmentsLocal[0]
+    x0, y0 = x1, y1
     for segment in segmentsLocal[1:]:
         x2, y2 = segment
         if x <= x2:
             return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+        x0, y0 = x1, y1
         x1, y1 = x2, y2
+    if defaultRight == "extrapolate":
+        return y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+    return defaultRight
 
 
 def line(slope, c, x):
@@ -236,6 +457,25 @@ def gen():
     xs = range(1, 11)
     points = list(zip(xs, ys))
     print(json.dumps(points))
+
+
+def findPositionCost():
+    low = segments["low"]["pos"][:2]
+    high = segments["high"]["pos"][:2]
+    dy = 2.5*math.sin(math.pi/4)
+    dx = 2.5*math.cos(math.pi/4)
+    x1, y1, x2, y2 = low[0] - dx, low[1] + dy, high[0] + dx, high[1] - dy
+    d = dist(x1, y1, x2, y2)
+    costPerD = 9 / d
+    print(dx, dy)
+    # (0.732 19.268)
+    print(x1, y1)
+
+    # 9.268 10.732
+    print(x2, y2)
+
+    print(costPerD)
+    # 0.7456
 
 
 if __name__ == '__main__':
